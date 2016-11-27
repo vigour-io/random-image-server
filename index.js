349ef3a63884d02fabb5303973ced40249442a95
Youri Daamen <me@youz.io>
2016-11-27 18:37:23 -0500

    removed image resizing

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const files = require('fs').readdirSync('img')
-const sharp = require('sharp')
+const fs = require('fs')
+const files = fs.readdirSync('img')
 const http = require('http')
 const port = process.env.NOW ? 80 : 8888
 
 http.createServer((req, res) => {
   const path = `img/${files[Math.floor(Math.random() * files.length)]}`
-  sharp(path)
-    .resize(400)
-    .toBuffer()
-    .then(data => {
-      res.writeHead(200, { 'Content-Type': 'image/jpeg' })
-      res.end(data, 'binary')
-    }).catch(e => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
       res.writeHead(404, { 'Content-Type': 'text/plain' })
       res.end(`Error handling: ${path}\n`)
-    })
+    } else {
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' })
+      res.end(data, 'binary')
+    }
+  })
 }).listen(port)
 console.log('listening on port:', port)
